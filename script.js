149efbeff07ed682e4ac139e4252cc31d0fc772d
hindustries <100952092+hindustries@users.noreply.github.com>
2022-03-08 20:38:08 +0100

    setup video

--- script.js
+++ script.js
@@ -2,40 +2,57 @@ let section = document.querySelectorAll("section");
 let menu = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  section.forEach((i) => {
-    let top = window.scrollY;
-    let offset = i.offsetTop - 150;
-    let height = i.offsetHeight;
-    let id = i.getAttribute("id");
+    section.forEach((i) => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+        let id = i.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
+        if (top >= offset && top < offset + height) {
+            menu.forEach((link) => {
+                link.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        }
+    });
 };
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+const registerVideo = (bound, video) => {
+    bound = document.querySelector(bound);
+    video = document.querySelector(video);
+    const scrollVideo = () => {
+        if (video.duration) {
+            const distanceFromTop = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+            const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+            const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
+            video.currentTime = video.duration * percentScrolled;
+        }
+        requestAnimationFrame(scrollVideo);
     }
-  }
+    requestAnimationFrame(scrollVideo);
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
 }
 
 window.addEventListener("scroll", reveal);
+registerVideo("#bound-one", "#bound-one video")
 
 // To check the scroll position on page load
 reveal();
